e867a65c6e661349f1688c280275e46b0a446cf7
yuanming <1550290560@qq.com>
2019-01-24 22:02:14 +0800

    本次修改加入了一个新方法

--- src/yuanfeng/helloworld.java
+++ src/yuanfeng/helloworld.java
@@ -4,4 +4,8 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("与英雄同路，为梦想轰鸣！");
     }
+    public void Test(){
+        int i = 0;
+        i++;
+    }
 }
